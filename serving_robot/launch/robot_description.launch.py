68f332a712f9282cedd053908185eda1ccc72247
Streetbell <ram@streetbell.com>
2026-09-14 00:34:47 +0530

    fix: publish the actual robot URDF

--- serving_robot/launch/robot_description.launch.py
+++ serving_robot/launch/robot_description.launch.py
@@ -1,17 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution, Command
+from launch.substitutions import PathJoinSubstitution
 
 
 def generate_launch_description():
-    urdf = PathJoinSubstitution([FindPackageShare('serving_robot'), 'urdf', 'serving_robot.urdf'])
-    description = Command(['cat ', urdf])
+    urdf_path = PathJoinSubstitution([FindPackageShare('serving_robot'), 'urdf', 'serving_robot.urdf'])
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            parameters=[{'robot_description': description}],
+            output='screen',
+            arguments=[urdf_path],
         )
     ])
