a3e3d471033888341c1ccac6eb1485a570e38d5e
Streetbell <ram@streetbell.com>
2026-09-14 00:34:33 +0530

    chore: bump package to v0.3.0

--- serving_robot/setup.py
+++ serving_robot/setup.py
@@ -5,7 +5,7 @@ package_name = 'serving_robot'
 
 setup(
     name=package_name,
-    version='0.2.0',
+    version='0.3.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
