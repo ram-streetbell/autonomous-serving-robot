5f30763f33643b956926a40fe290654260f94e80
Streetbell <ram@streetbell.com>
2026-09-14 00:31:18 +0530

    fix: load URDF into robot state publisher

--- serving_robot/launch/robot_description.launch.py
+++ serving_robot/launch/robot_description.launch.py
@@ -1,16 +1,17 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, Command
 
 
 def generate_launch_description():
     urdf = PathJoinSubstitution([FindPackageShare('serving_robot'), 'urdf', 'serving_robot.urdf'])
+    description = Command(['cat ', urdf])
     return LaunchDescription([
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             name='robot_state_publisher',
-            parameters=[{'robot_description': open('/dev/null').read()}],
+            parameters=[{'robot_description': description}],
         )
     ])
